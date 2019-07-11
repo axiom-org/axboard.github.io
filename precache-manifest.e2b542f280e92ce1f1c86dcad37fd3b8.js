8dfac945bb9f4655aad385e6a32a4ce70808b0bd
Kevin Lacker <lacker@gmail.com>
2019-07-11 11:32:26 -0700

    Updates

---
+++ precache-manifest.e2b542f280e92ce1f1c86dcad37fd3b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e40b5c67198e5d4aad4450d8c21c11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9883a3f8bb24d0eadcd",
+    "url": "/static/css/main.4c09c131.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "a9883a3f8bb24d0eadcd",
+    "url": "/static/js/main.bfd9492c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.e2b542f280e92ce1f1c86dcad37fd3b8.js"
 );
 
 self.addEventListener('message', (event) => {
